f2d1eefffe74c7f48540e389b648cd06cefb5016
Aldvinbek <aldvin@mail.ru>
2019-10-11 12:42:59 +0300

    Convert collider point to texture point

--- udf/Assets/mine_texture.cs
+++ udf/Assets/mine_texture.cs
@@ -49,6 +49,28 @@ public class mine_texture : MonoBehaviour
                     newTex.SetPixel((int)coll.transform.position.x + i - 56, (int)coll.transform.position.y + j + (int)transform.position.y - 228, pixels2cut[i, j]);
                 }
             }
+
+            // Точка соприкосновения на основе ContactPoint-ов объекта Collision (мировые координаты)
+            // Не использую, просто для примера, т.к. по хорошему от коллайдеров лучше избавиться (но это на твой выбор)
+            var contactPosition = coll.contacts[0].point;
+            
+            // Позиция объекта с коллайдером в мировых координатах
+            var collGlobalPos = coll.transform.TransformPoint(Vector3.zero);
+            
+            // contactPosition и collGlobalPos естественно совпадают
+            
+            // Позиция себя(нижнего левого угла текстуры) в мировых координатах
+            var thisGlobalPos = transform.TransformPoint(-tex.width / 2, -tex.height / 2, 0);
+            
+            // Координата на текстуре (разница между collGlobalPos и thisGlobalPos)
+            int x = (int)(collGlobalPos.x - thisGlobalPos.x);
+            int y = (int)(collGlobalPos.y - thisGlobalPos.y);
+            
+            // Ставим красный пиксель 
+            newTex.SetPixel(x, y, new Color32(255, 0, 0, 255));
+            
+            
+            
             newTex.Apply();
             Sprite newSprite = Sprite.Create(newTex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f,0.5f), 1f);
             rend.sprite = newSprite;

b68d5101d4975c3ca1e1be5f33b12b56dfa4c840
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2019-10-18 00:31:40 +0300

    Mine drawing

--- udf/Assets/Mine.cs
+++ udf/Assets/Mine.cs
@@ -57,7 +57,7 @@ public class Mine : MonoBehaviour
         _last_ySPR += speed;
         _yCPoffset = Random.Range(50, 100);
         _yCP -= _yCPoffset;
-        if (_yCP > -800) GenerateVertebra(_xCP, _yCP);
+        if (_yCP > -1200) GenerateVertebra(_xCP, _yCP);
         else {
             SidesSpawnTrigger = 1;
             _yCP += _yCPoffset;
@@ -81,14 +81,14 @@ public class Mine : MonoBehaviour
         if (_xCP < _CPPosLimitL) _xCP = _CPPosLimitL;
         if (_xCP > _CPPosLimitR) _xCP = _CPPosLimitR;
         _xSPL = _xCP - 50 - Random.Range(0, 150);
-        if (_xSPL < _SPPosLimitL) _xSPL = _SPPosLimitL;
+        _xSPL = Mathf.Clamp(_xSPL, _SPPosLimitL, 255);
         if (_mineDictNumber > 0) while (Mathf.Abs(_xSPL - _mineDict[_mineDictNumber - 1][2].transform.position.x) < SPConvLimit) _xSPL -= 10; //при необходимости двигаем x-координату конца ребра от центра, чтобы избежать экстремальных изломов шахты
         _ySPL = _yCP + Random.Range(-25, 25);
         if (_last_ySPL == MainCamera.pixelHeight * 2) _last_ySPL = _ySPL; // выставляем первое реальное значение y-координаты конца ребра для запоминания
         if (_ySPL > _last_ySPL) _ySPL = _last_ySPL; // проверяем, не пересекаются ли рёбра, и если да, исправляем
         _last_ySPL = _ySPL; // запоминаем у-коррдинату конца ребра, чтобы при следующей генерации позвонка проверить, не пересекутся ли рёбра
         _xSPR = _xCP + 50 + Random.Range(0, 150);
-        if (_xSPR > _SPPosLimitR) _xSPR = _SPPosLimitR;
+        _xSPR = Mathf.Clamp(_xSPR, 256, _SPPosLimitR);
         if (_mineDictNumber > 0) while (Mathf.Abs(_xSPR - _mineDict[_mineDictNumber - 1][1].transform.position.x) < SPConvLimit) _xSPR += 10; //при необходимости двигаем x-координату конца ребра от центра, чтобы избежать экстремальных изломов шахты
         _ySPR = _yCP + Random.Range(-25, 25);
         if (_last_ySPR == MainCamera.pixelHeight * 2) _last_ySPR = _ySPR; // выставляем первое реальное значение y-координаты конца ребра для запоминания
--- udf/Assets/Sides.cs
+++ udf/Assets/Sides.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 public class Sides : MonoBehaviour
 {
     SpriteRenderer rend;
+    float yn_dist;
+    float tgA;
+    float a;
+    float x;
+    float y;
+    float x_next;
+    float y_next;
     public Dictionary<int, Dictionary<int, GameObject>> _mineDict;
     public void Start()
     {
@@ -19,31 +26,65 @@ public class Sides : MonoBehaviour
         _mineDict = GameObject.Find("Controller").GetComponent<Mine>()._mineDict;
         foreach (KeyValuePair<int, Dictionary<int, GameObject>> vertebra in _mineDict)
         {
-            for (int a = 1; a < 3; a++) {
-                if (vertebra.Value[a].transform.position.y > GlobalPos.y && vertebra.Value[a].transform.position.y < (GlobalPos.y + tex.height))
+            //for (int a = 1; a < 3; a++) {
+                if (vertebra.Value[1].transform.position.y > GlobalPos.y && vertebra.Value[1].transform.position.y < (GlobalPos.y + tex.height))
                 {
-                    int x = (int)(vertebra.Value[a].transform.position.x - GlobalPos.x);
-                    int y = (int)(vertebra.Value[a].transform.position.y - GlobalPos.y);
-                    Color32[,] pixels2cut = new Color32[5, 5];
-                    int rows2cut = pixels2cut.GetUpperBound(0) + 1;
-                    int columns2cut = pixels2cut.Length / rows2cut;
-                    for (int i = 0; i < rows2cut; i++)
+                x = vertebra.Value[1].transform.position.x - GlobalPos.x; //горизонталь точки, правее которой будем удалять текстуру
+                y = vertebra.Value[1].transform.position.y - GlobalPos.y; //вертикаль точки, правее которой будем удалять текстуру
+                x_next = _mineDict[vertebra.Key + 1][1].transform.position.x - GlobalPos.x; //горизонталь точки на следующем позвонке шахты
+                y_next = _mineDict[vertebra.Key + 1][1].transform.position.y - GlobalPos.y; //вертикаль точки на следующем позвонке шахты
+                Color32[,] pixels2cut_triangle = new Color32[(int)Mathf.Abs(x_next - x), (int)(y - y_next)];
+                int rows2cut_triangle = pixels2cut_triangle.GetUpperBound(0) + 1;
+                if (rows2cut_triangle != 0) { 
+                    int columns2cut_triangle = pixels2cut_triangle.Length / rows2cut_triangle;
+                    for (int i = 0; i < rows2cut_triangle; i++)
                     {
-                        for (int j = 0; j < columns2cut; j++)
+                        for (int j = 0; j < columns2cut_triangle; j++)
                         {
-                            pixels2cut[i, j].r = 0;
-                            pixels2cut[i, j].g = 0;
-                            pixels2cut[i, j].b = 0;
-                            pixels2cut[i, j].a = 0;
-                            newTex.SetPixel(x + i, y + j, pixels2cut[i, j]);
+                            if (x < x_next) {
+                                yn_dist = y - j - y_next;
+                                tgA = (x_next - x) / (y - y_next);
+                                a = tgA * yn_dist;
+                                //print(yn_dist + " " + tgA + " " +a);
+                                if (x_next - i > x_next - a) {
+                                    pixels2cut_triangle[i, j].a = 0;
+                                    newTex.SetPixel((int)x_next - i, (int)y - j, pixels2cut_triangle[i, j]);
+                                }
+                            }
+                            if (x > x_next)
+                            {
+                                yn_dist = y - j - y_next;
+                                tgA = (x - x_next) / (y - y_next);
+                                a = tgA * yn_dist;
+                                if (x_next - i > x_next - a)
+                                {
+                                    pixels2cut_triangle[i, j].a = 0;
+                                    newTex.SetPixel((int)x - i, (int)y_next + j, pixels2cut_triangle[i, j]);
+                                }
+                            }
+                            //else newTex.SetPixel(x - i, y - j, pixels2cut_triangle[i, j]);
                         }
                     }
                 }
-                newTex.Apply();
-                Sprite newSprite = Sprite.Create(newTex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 1f);
-                rend.sprite = newSprite;
+                if (x < x_next) x = x_next; //корректировка горизонтали для случаев, когда следующая ключевая точка шахты находится правее (чтобы не удалять текстуру вне шахты)
+                Color32[,] pixels2cut_rect = new Color32[(int)(256 - x), (int)(y - y_next)];
+                //print(256 - x + " " + (y - (int)(_mineDict[vertebra.Key + 1][1].transform.position.y - GlobalPos.y)));
+                int rows2cut_rect = pixels2cut_rect.GetUpperBound(0) + 1;
+                int columns2cut_rect = pixels2cut_rect.Length / rows2cut_rect;
+                for (int i = 0; i < rows2cut_rect; i++)
+                {
+                    for (int j = 0; j < columns2cut_rect; j++)
+                    {
+                        pixels2cut_rect[i, j].a = 0;
+                        newTex.SetPixel(256 - i, (int)y - j, pixels2cut_rect[i, j]);
+                    }
+                }
+                //} _mineDict[vertebra.Key - 1][0].transform.position, vertebra.Value[0].transform.position
             }
         }
+        newTex.Apply();
+        Sprite newSprite = Sprite.Create(newTex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 1f);
+        rend.sprite = newSprite;
     }
 
 

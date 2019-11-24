b7ccfa195ad89970b4b7c537b0ee652932ce5381
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2019-11-24 04:10:58 +0300

    g avoiding obstacles

--- udf/Assets/g.cs
+++ udf/Assets/g.cs
@@ -4,37 +4,24 @@ using UnityEngine;
 
 public class g : MonoBehaviour
 {
-
+    private float speed;
     [SerializeField] public GameObject b;
     float next_x;
     float angle;
     private Dictionary<int, Dictionary<int, GameObject>> _mineDict;
-    void Start()
-    {
-
-    }
-
     void Update()
     {
-        _mineDict = GameObject.Find("Controller").GetComponent<Mine>()._mineDict;
-        foreach (KeyValuePair<int, Dictionary<int, GameObject>> vertebra in _mineDict) {
-           if (vertebra.Value[0].transform.position.y > transform.position.y && _mineDict[vertebra.Key + 1][0].transform.position.y < transform.position.y) {
-                next_x = transform.position.x + (_mineDict[vertebra.Key + 1][0].transform.position.x - transform.position.x) / (transform.position.y - _mineDict[vertebra.Key + 1][0].transform.position.y);
-                angle = Mathf.Atan((_mineDict[vertebra.Key + 1][0].transform.position.x - vertebra.Value[0].transform.position.x) / (vertebra.Value[0].transform.position.y - _mineDict[vertebra.Key + 1][0].transform.position.y)) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + angle)), 360);
-            }
+        speed = b.GetComponent<B>().vertSpeed;
+        if (transform.position.y>780) {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
         }
-        transform.position = new Vector3(next_x, transform.position.y, transform.position.z);
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        StartCoroutine(CollisionExit());
+    }
+    IEnumerator CollisionExit() {
+        yield return new WaitForSeconds(2);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0, 0, 0)), 360);
     }
 }
-
-
-
-
-
-
-
-
-
-
-    

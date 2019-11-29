0642b729e4e07147727302eec56cdc429827dfd6
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2019-11-30 01:10:45 +0300

    Magic shots spawning

--- udf/Assets/g.cs
+++ udf/Assets/g.cs
@@ -6,14 +6,20 @@ public class g : MonoBehaviour
 {
     private float speed;
     [SerializeField] public GameObject b;
+    [SerializeField] public GameObject s;
     float next_x;
     float angle;
     private Dictionary<int, Dictionary<int, GameObject>> _mineDict;
+    int fire_cooldown_trigger;
     void Update()
     {
         speed = b.GetComponent<B>().vertSpeed;
         if (transform.position.y>780) {
             transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        }
+        if (fire_cooldown_trigger == 0) {
+            fire_cooldown_trigger = 1;
+            StartCoroutine(Fire());
         }
     }
     void OnCollisionExit2D(Collision2D other)
@@ -23,5 +29,11 @@ public class g : MonoBehaviour
     IEnumerator CollisionExit() {
         yield return new WaitForSeconds(2);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0, 0, 0)), 360);
+    }
+    IEnumerator Fire() {
+        yield return new WaitForSeconds(2);
+        var snowball = Instantiate(s, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        snowball.transform.parent = transform;
+        fire_cooldown_trigger = 0;
     }
 }

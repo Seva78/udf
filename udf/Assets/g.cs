e161378385f35916a6824718f42f2014768b60a8
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2019-12-01 04:03:07 +0300

    snowballs test

--- udf/Assets/g.cs
+++ udf/Assets/g.cs
@@ -17,7 +17,8 @@ public class g : MonoBehaviour
         if (transform.position.y>780) {
             transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
         }
-        if (fire_cooldown_trigger == 0) {
+        if (fire_cooldown_trigger == 0)
+        {
             fire_cooldown_trigger = 1;
             StartCoroutine(Fire());
         }
@@ -30,10 +31,10 @@ public class g : MonoBehaviour
         yield return new WaitForSeconds(2);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(0, 0, 0)), 360);
     }
-    IEnumerator Fire() {
+    IEnumerator Fire()
+    {
         yield return new WaitForSeconds(2);
         var snowball = Instantiate(s, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-        snowball.transform.parent = transform;
         fire_cooldown_trigger = 0;
     }
 }
---
+++ udf/Assets/s.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s : MonoBehaviour
+{
+    GameObject b;
+    Vector3 b_position;
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        b = GameObject.Find("b");
+        speed = b.GetComponent<B>().vertSpeed;
+        b_position = b.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - (transform.position.x - b_position.x) / 1000, transform.position.y - speed - 0.005f, transform.position.z);
+    }
+}

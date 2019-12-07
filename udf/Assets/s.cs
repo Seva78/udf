31332b2382d5922143cce614b73818cd7cbc4b20
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2019-12-07 23:12:19 +0300

    projectiles with random trajectory

--- udf/Assets/s.cs
+++ udf/Assets/s.cs
@@ -5,20 +5,35 @@ using UnityEngine;
 public class s : MonoBehaviour
 {
     GameObject b;
+    GameObject g;
     Vector3 b_position;
-    float speed;
+    Vector3 g_position;
+    int change_trajectory_trigger;
+    int change_trajectory_value;
 
-    // Start is called before the first frame update
     void Start()
     {
         b = GameObject.Find("b");
-        speed = b.GetComponent<B>().vertSpeed;
+        g = GameObject.Find("g");
         b_position = b.transform.position;
+        g_position = g.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x - (transform.position.x - b_position.x) / 1000, transform.position.y - speed - 0.005f, transform.position.z);
+        transform.position = new Vector3(transform.position.x - (g_position.x - (b_position.x + change_trajectory_value)) / 50, transform.position.y - (g_position.y - b_position.y) / 50, transform.position.z);
+        if (change_trajectory_trigger == 0) {
+            change_trajectory_trigger = 1;
+            StartCoroutine("ChangeTrajectory");
+        }
+    }
+    IEnumerator ChangeTrajectory()
+    {
+        yield return new WaitForSeconds(0.4f);
+        change_trajectory_value = Random.Range(300,-300);
+        change_trajectory_trigger = 0;
+    }
+    void OnCollisionEnter2D() {
+        Destroy(gameObject);
     }
 }

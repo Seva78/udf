f16650257d0ae5a1a313a7fb9685066faebd6185
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2019-12-28 02:00:06 +0300

    GUI

---
+++ udf/Assets/music_button.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class music_button : MonoBehaviour
+{
+    public int counter = 0;
+    public Button music;
+    public Sprite music_off;
+    public Sprite music_on;
+    public void changePic()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            music.GetComponent<Image>().sprite = music_off;
+        }
+        else music.GetComponent<Image>().sprite = music_on;
+    }
+}
---
+++ udf/Assets/snd_button.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class snd_button : MonoBehaviour
+{
+    public int counter = 0;
+    public Button snd;
+    public Sprite snd_off;
+    public Sprite snd_on;
+    public void changePic() {
+        counter++;
+        if (counter % 2 == 1) {
+            snd.GetComponent<Image>().sprite = snd_off;
+        } else snd.GetComponent<Image>().sprite = snd_on;
+    }
+}

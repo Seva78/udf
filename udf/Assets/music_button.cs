4eecdc9d7d3ff1ddc33b8316720eb865b2951925
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2019-12-28 23:51:04 +0300

    new texture, audio

--- udf/Assets/music_button.cs
+++ udf/Assets/music_button.cs
@@ -7,13 +7,18 @@ public class music_button : MonoBehaviour
     public Button music;
     public Sprite music_off;
     public Sprite music_on;
+    public GameObject controller;
     public void changePic()
     {
         counter++;
         if (counter % 2 == 1)
         {
             music.GetComponent<Image>().sprite = music_off;
+            controller.GetComponent<AudioSource>().mute = true;
         }
-        else music.GetComponent<Image>().sprite = music_on;
+        else {
+            music.GetComponent<Image>().sprite = music_on;
+            controller.GetComponent<AudioSource>().mute = false;
+        }
     }
 }

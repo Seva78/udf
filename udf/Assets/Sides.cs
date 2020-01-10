7ac250351009444bb16b90ab01e1a12dec2c6b4c
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2020-01-11 00:05:53 +0300

    file names

--- udf/Assets/Sides.cs
+++ udf/Assets/Sides.cs
@@ -3,84 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using System.Linq;
-
-[RequireComponent(typeof(SpriteRenderer))]
 public class Sides : MonoBehaviour
 {
-    private float y_top;
-    private int wall;
-    private int wall_left;
-    private int wall_right;
-    private int mine_width;
-    private int _mineDictNumberLocal;
-    //    SpriteRenderer rend;
-    SpriteMask rend;
-    private Dictionary<int, Dictionary<int, GameObject>> _mineDict;
-    private Dictionary<int, GameObject> _vertebraDictLocal;
-    private Dictionary<int, Dictionary<int, GameObject>> _mineDictLocal;
-    public void Start()
-    {
-        //        rend = GetComponent<SpriteRenderer>();
-        rend = GetComponent<SpriteMask>();
-        Texture2D tex = rend.sprite.texture;
-        Color32[] pixels = new Color32[tex.width * tex.height];
-        pixels = tex.GetPixels32();
-        Texture2D newTex = new Texture2D(tex.width, tex.height, tex.format, mipChain: false);
-        newTex.SetPixels32(pixels);
-        Vector3 GlobalPos = transform.TransformPoint(-tex.width / 2, -tex.height / 2, 0);
-        y_top = GlobalPos.y + tex.height; //вертикальная координата верхней части объекта 
-        _mineDict = GameObject.Find("Controller").GetComponent<Mine>()._mineDict;
-        _mineDictLocal = new Dictionary<int, Dictionary<int, GameObject>>();
-        for (int i = _mineDict.Keys.Min() + 1; i < _mineDict.Keys.Max(); i++) {
-            if (_mineDict[i + 1][1].transform.position.y < y_top && _mineDict[i + 1][1].transform.position.y > GlobalPos.y ||
-                _mineDict[i - 1][1].transform.position.y < y_top && _mineDict[i - 1][1].transform.position.y > GlobalPos.y ||
-                _mineDict[i + 1][2].transform.position.y < y_top && _mineDict[i + 1][2].transform.position.y > GlobalPos.y ||
-                _mineDict[i - 1][2].transform.position.y < y_top && _mineDict[i - 1][2].transform.position.y > GlobalPos.y)
-            {
-                _vertebraDictLocal = new Dictionary<int, GameObject>();
-                _vertebraDictLocal.Add(0, _mineDict[i][0]);
-                _vertebraDictLocal.Add(1, _mineDict[i][1]);
-                _vertebraDictLocal.Add(2, _mineDict[i][2]);
-                _mineDictLocal.Add(_mineDictNumberLocal, _vertebraDictLocal);
-                _mineDictNumberLocal++;
-            }
-        }
-        for (int y = (int)GlobalPos.y; y < (int)y_top; y++)
-        {
-            foreach (KeyValuePair<int, Dictionary<int, GameObject>> vertebra in _mineDictLocal)
-            {
-                for (int q = 1; q < 3; q++)
-                { //q принимает значения 1 и 2; 1 - это значения координат ключевых точек по левой стороне шахты, а 2 - по правой
-                    if (vertebra.Value[q].transform.position.y > y && _mineDictLocal[vertebra.Key + 1][q].transform.position.y < y)
-                    {
-                        wall = (int)(vertebra.Value[q].transform.position.x - (vertebra.Value[q].transform.position.x -
-                            _mineDictLocal[vertebra.Key + 1][q].transform.position.x) * ((vertebra.Value[q].transform.position.y - y) / (vertebra.Value[q].transform.position.y - _mineDictLocal[vertebra.Key + 1][q].transform.position.y)));
-                        if (q == 1) wall_left = wall;
-                        else wall_right = wall;
-                    }
-                }
-            }
-            mine_width = wall_right - wall_left;
-            newTex.SetPixels32(wall_left, y - (int)GlobalPos.y, mine_width, 1, GetRow(mine_width));
-        }
-        newTex.Apply();
-        Sprite newSprite = Sprite.Create(newTex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 1f);
-        rend.sprite = newSprite;
-    }
-
-    public Color32[] GetRow(int length)
-    {
-        var pixel = new Color32(0, 0, 0, 0);
-        var row = new Color32[length];
-        for (int i = 0; i < length; i++)
-        {
-            row[i] = pixel;
-        }
-        return row;
-    }
-
+    private float speed;
     public void Update()
     {
+        speed = transform.parent.gameObject.GetComponent<Mine>().speed;
+        transform.position = new Vector3(transform.position.x, transform.position.y + speed / transform.parent.gameObject.GetComponent<Texture>().sidesLagCoeff, transform.position.z);
         if (transform.position.y > 1060) Destroy(gameObject);
     }
 }
--- udf/Assets/Texture.cs
+++ udf/Assets/Texture.cs
@@ -4,53 +4,65 @@ using UnityEngine;
 
 public class Texture : MonoBehaviour
 {
+    [SerializeField] public GameObject TileMask;
     [SerializeField] public GameObject TileSides;
-    [SerializeField] public GameObject TileSidesTexture;
     [SerializeField] public GameObject TileBackground;
     [SerializeField] public Camera MainCamera;
     [SerializeField] public GameObject b;
     public float backgroundLagCoeff; //отставание текстуры бэкграунда от маски
     public float sidesLagCoeff; //отставание текстуры стенок от маски
     private float speed;
+    private float _yMaskTile;
     private float _ySidesTile;
-    private float _ySidesTextureTile;
     private float _yBackgroundTile;
+    private Dictionary<int, GameObject> _tileMaskDict;
     private Dictionary<int, GameObject> _tileSidesDict;
-    private Dictionary<int, GameObject> _tileSidesTextureDict;
     private Dictionary<int, GameObject> _tileBackgroundDict;
+    private int _tileMaskDictNumber;
     private int _tileSidesDictNumber;
-    private int _tileSidesTextureDictNumber;
     private int _tileBackgroundDictNumber;
+    private int _tileMaskToDelete;
     private int _tileSidesToDelete;
-    private int _tileSidesTextureToDelete;
     private int _tileBackgroundToDelete;
     void Start()
     {
+        _tileMaskDict = new Dictionary<int, GameObject>();
         _tileSidesDict = new Dictionary<int, GameObject>();
-        _tileSidesTextureDict = new Dictionary<int, GameObject>();
         _tileBackgroundDict = new Dictionary<int, GameObject>();
+        _yMaskTile = MainCamera.pixelHeight + 256;
         _ySidesTile = MainCamera.pixelHeight + 256;
-        _ySidesTextureTile = MainCamera.pixelHeight + 256;
         _yBackgroundTile = MainCamera.pixelHeight + 256;
+        GenerateMaskTile(256, _yMaskTile);
         GenerateSidesTile(256, _ySidesTile);
-        GenerateSidesTextureTile(256, _ySidesTile);
         GenerateBackgroundTile(256, _yBackgroundTile);
     }
     void Update()
     {
         speed = b.GetComponent<B>().vertSpeed;
-        _ySidesTile += speed;
-        _ySidesTextureTile += speed + speed / sidesLagCoeff;
+        _yMaskTile += speed;
+        _ySidesTile += speed + speed / sidesLagCoeff;
         _yBackgroundTile += speed - speed / backgroundLagCoeff;
-        _ySidesTile -= TileSides.GetComponent<SpriteMask>().sprite.texture.height;
-        _ySidesTextureTile -= TileSidesTexture.GetComponent<SpriteRenderer>().sprite.texture.height;
+        _yMaskTile -= TileMask.GetComponent<SpriteMask>().sprite.texture.height;
+        _ySidesTile -= TileSides.GetComponent<SpriteRenderer>().sprite.texture.height;
         _yBackgroundTile -= TileBackground.GetComponent<SpriteRenderer>().sprite.texture.height * TileBackground.transform.localScale.y;
-        if (_ySidesTile > -TileSides.GetComponent<SpriteMask>().sprite.texture.height && gameObject.GetComponent<Mine>().TextureSpawnTrigger == 1) GenerateSidesTile(256, _ySidesTile);
-        else _ySidesTile += TileSides.GetComponent<SpriteMask>().sprite.texture.height;
-        if (_ySidesTextureTile > -TileSidesTexture.GetComponent<SpriteRenderer>().sprite.texture.height && gameObject.GetComponent<Mine>().TextureSpawnTrigger == 1) GenerateSidesTextureTile(256, _ySidesTextureTile);
-        else _ySidesTextureTile += TileSidesTexture.GetComponent<SpriteRenderer>().sprite.texture.height;
+        if (_yMaskTile > -TileMask.GetComponent<SpriteMask>().sprite.texture.height && gameObject.GetComponent<Mine>().TextureSpawnTrigger == 1) GenerateMaskTile(256, _yMaskTile);
+        else _yMaskTile += TileMask.GetComponent<SpriteMask>().sprite.texture.height;
+        if (_ySidesTile > -TileSides.GetComponent<SpriteRenderer>().sprite.texture.height && gameObject.GetComponent<Mine>().TextureSpawnTrigger == 1) GenerateSidesTile(256, _ySidesTile);
+        else _ySidesTile += TileSides.GetComponent<SpriteRenderer>().sprite.texture.height;
         if (_yBackgroundTile > -TileBackground.GetComponent<SpriteRenderer>().sprite.texture.height * TileBackground.transform.localScale.y && gameObject.GetComponent<Mine>().TextureSpawnTrigger == 1) GenerateBackgroundTile(256, _yBackgroundTile);
         else _yBackgroundTile += TileBackground.GetComponent<SpriteRenderer>().sprite.texture.height * TileBackground.transform.localScale.y;
+        foreach (KeyValuePair<int, GameObject> tile in _tileMaskDict)
+        {
+            if (tile.Value.transform.position.y > MainCamera.pixelHeight + 200)
+            {
+                _tileMaskToDelete = tile.Key + 1;
+            }
+        }
+        if (_tileMaskToDelete != 0)
+        {
+            _tileMaskDict.Remove(_tileMaskToDelete - 1);
+            _tileMaskToDelete = 0;
+        }
         foreach (KeyValuePair<int, GameObject> tile in _tileSidesDict)
         {
             if (tile.Value.transform.position.y > MainCamera.pixelHeight + 200)
@@ -62,18 +74,6 @@ public class Texture : MonoBehaviour
         {
             _tileSidesDict.Remove(_tileSidesToDelete - 1);
             _tileSidesToDelete = 0;
-        }
-        foreach (KeyValuePair<int, GameObject> tile in _tileSidesTextureDict)
-        {
-            if (tile.Value.transform.position.y > MainCamera.pixelHeight + 200)
-            {
-                _tileSidesTextureToDelete = tile.Key + 1;
-            }
-        }
-        if (_tileSidesTextureToDelete != 0)
-        {
-            _tileSidesTextureDict.Remove(_tileSidesTextureToDelete - 1);
-            _tileSidesTextureToDelete = 0;
         }
         foreach (KeyValuePair<int, GameObject> tile in _tileBackgroundDict)
         {
@@ -88,27 +88,27 @@ public class Texture : MonoBehaviour
             _tileBackgroundToDelete = 0;
         }
     }
+    void GenerateMaskTile(int _xTile, float _yMaskTile)
+    {
+        var TileMaskI = Instantiate(TileMask, new Vector3(_xTile, _yMaskTile, 0), Quaternion.identity);
+        TileMaskI.transform.parent = transform;
+        TileMaskI.name = "Mask" + _tileMaskDictNumber;
+        _tileMaskDict.Add(_tileMaskDictNumber, TileMaskI);
+        _tileMaskDictNumber++;
+    }
     void GenerateSidesTile(int _xTile, float _ySidesTile)
     {
         var TileSidesI = Instantiate(TileSides, new Vector3(_xTile, _ySidesTile, 0), Quaternion.identity);
         TileSidesI.transform.parent = transform;
-        TileSidesI.name = "Mask" + _tileSidesDictNumber;
+        TileSidesI.name = "Side" + _tileSidesDictNumber;
         _tileSidesDict.Add(_tileSidesDictNumber, TileSidesI);
         _tileSidesDictNumber++;
-    }
-    void GenerateSidesTextureTile(int _xTile, float _ySidesTextureTile)
-    {
-        var TileSidesTextureI = Instantiate(TileSidesTexture, new Vector3(_xTile, _ySidesTextureTile, 0), Quaternion.identity);
-        TileSidesTextureI.transform.parent = transform;
-        TileSidesTextureI.name = "SideTexture" + _tileSidesTextureDictNumber;
-        _tileSidesTextureDict.Add(_tileSidesTextureDictNumber, TileSidesTextureI);
-        _tileSidesTextureDictNumber++;
     }
     void GenerateBackgroundTile(int _xTile, float _yBackgroundTile)
     {
         var TileBackgroundI = Instantiate(TileBackground, new Vector3(_xTile, _yBackgroundTile, 0), Quaternion.identity);
         TileBackgroundI.transform.parent = transform;
-        TileBackgroundI.name = "BackgroundTexture" + _tileBackgroundDictNumber;
+        TileBackgroundI.name = "Background" + _tileBackgroundDictNumber;
         _tileBackgroundDict.Add(_tileBackgroundDictNumber, TileBackgroundI);
         _tileBackgroundDictNumber++;
     }

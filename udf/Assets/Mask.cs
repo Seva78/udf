04319ac165ace7387004e8188ab1e933bdf1d4bf
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2020-02-25 02:58:39 +0300

    refactoring

--- udf/Assets/Mask.cs
+++ udf/Assets/Mask.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 using System.Linq;
 public class Mask : MonoBehaviour
 {
-    private int wall_left;
-    private int wall_right;
     public void Start()
     {
         var rend = GetComponent<SpriteMask>();
@@ -16,54 +14,59 @@ public class Mask : MonoBehaviour
         Texture2D newTex = new Texture2D(tex.width, tex.height, tex.format, mipChain: false);
         newTex.SetPixels32(pixels);
         Vector3 GlobalPos = transform.TransformPoint(-tex.width / 2, -tex.height / 2, 0);
-        var y_top = GlobalPos.y + tex.height; //вертикальная координата верхней части объекта 
+        var yTop = GlobalPos.y + tex.height; //вертикальная координата верхней части объекта 
         var controller = GameObject.Find("Controller");
-        var _mineList = controller.GetComponent<Mine>()._mineList;
-        var _mineListLocal = new List<GameObject>();
-        for (var i = 1; i < _mineList.Count - 1; i++) {
-            var prevVert = _mineList[i - 1].GetComponent<Vertebra>();
-            var nextVert = _mineList[i + 1].GetComponent<Vertebra>();
-            if (nextVert.LeftY < y_top && nextVert.LeftY > GlobalPos.y ||
-                prevVert.LeftY < y_top && prevVert.LeftY > GlobalPos.y ||
-                nextVert.RightY < y_top && nextVert.RightY > GlobalPos.y ||
-                prevVert.RightY < y_top && prevVert.RightY > GlobalPos.y)
+        var mineList = controller.GetComponent<Mine>()._mineList;
+        var mineListLocal = new List<GameObject>();
+        for (var i = 1; i < mineList.Count - 1; i++) {
+            var prevVert = mineList[i - 1].GetComponent<Vertebra>();
+            var nextVert = mineList[i + 1].GetComponent<Vertebra>();
+            if (nextVert.LeftY < yTop && nextVert.LeftY > GlobalPos.y ||
+                prevVert.LeftY < yTop && prevVert.LeftY > GlobalPos.y ||
+                nextVert.RightY < yTop && nextVert.RightY > GlobalPos.y ||
+                prevVert.RightY < yTop && prevVert.RightY > GlobalPos.y)
             {
-                _mineListLocal.Add(_mineList[i]);
+                mineListLocal.Add(mineList[i]);
             }
         }
-        for (int y = (int)GlobalPos.y; y < (int)y_top; y++)
+        var wallLeft = 0;
+        var wallRight = 0;
+        for (int y = (int)GlobalPos.y; y < (int)yTop; y++)
         {
-            foreach (GameObject vertebra in _mineListLocal)
+            foreach (GameObject vertebra in mineListLocal)
             {
-                if (vertebra.GetComponent<Vertebra>().leftPoint.transform.position.y > y && _mineListLocal[_mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().leftPoint.transform.position.y < y)
+                if (vertebra.GetComponent<Vertebra>().leftPoint.transform.position.y > y && 
+                    mineListLocal[mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().leftPoint.transform.position.y < y)
                 {
-                    wall_left = wall_x_value(y, 
-                        vertebra.GetComponent<Vertebra>().leftPoint.transform.position.x,
-                        vertebra.GetComponent<Vertebra>().leftPoint.transform.position.y,
-                        _mineListLocal[_mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().leftPoint.transform.position.x,
-                        _mineListLocal[_mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().leftPoint.transform.position.y);
+                    var leftPoint = vertebra.GetComponent<Vertebra>().leftPoint.transform.position;
+                    var leftPointNext = 
+                        mineListLocal[mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().leftPoint.transform.position;
+                    wallLeft = WallXValue(y, leftPoint.x,leftPoint.y, 
+                        leftPointNext.x, leftPointNext.y);
                 }
-                if (vertebra.GetComponent<Vertebra>().rightPoint.transform.position.y > y && _mineListLocal[_mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().rightPoint.transform.position.y < y)
+                if (vertebra.GetComponent<Vertebra>().rightPoint.transform.position.y > y && 
+                    mineListLocal[mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().rightPoint.transform.position.y < y)
                 {
-                    wall_right = wall_x_value(y, 
-                        vertebra.GetComponent<Vertebra>().rightPoint.transform.position.x,
-                        vertebra.GetComponent<Vertebra>().rightPoint.transform.position.y,
-                        _mineListLocal[_mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().rightPoint.transform.position.x,
-                        _mineListLocal[_mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().rightPoint.transform.position.y);
+                    var rightPoint = vertebra.GetComponent<Vertebra>().rightPoint.transform.position;
+                    var rightPointNext = 
+                        mineListLocal[mineListLocal.IndexOf(vertebra) + 1].GetComponent<Vertebra>().rightPoint.transform.position;
+                    wallRight = WallXValue(y, rightPoint.x, rightPoint.y,
+                        rightPointNext.x, rightPointNext.y);
                 }
             }
-            var mine_width = wall_right - wall_left;
-            newTex.SetPixels32(wall_left, y - (int)GlobalPos.y, mine_width, 1, GetRow(mine_width));
+            var mineWidth = wallRight - wallLeft;
+            newTex.SetPixels32(wallLeft, y - (int)GlobalPos.y, mineWidth, 1, GetRow(mineWidth));
         }
         newTex.Apply();
-        Sprite newSprite = Sprite.Create(newTex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 1f);
+        Sprite newSprite = 
+            Sprite.Create(newTex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 1f);
         rend.sprite = newSprite;
     }
 
-    int wall_x_value(int y, float upper_keypoint_x, float upper_keypoint_y, float lower_keypoint_x, float lower_keypoint_y)
+    int WallXValue(int y, float upKeypointX, float upKeypointY, float lowKeypointX, float lowKeypointY)
     {
-        var wall_x = (int)(upper_keypoint_x - (upper_keypoint_x - lower_keypoint_x) * ((upper_keypoint_y - y) / (upper_keypoint_y - lower_keypoint_y)));
-        return wall_x;
+        var wallX = (int)(upKeypointX - (upKeypointX - lowKeypointX) * ((upKeypointY - y) / (upKeypointY - lowKeypointY)));
+        return wallX;
     }
 
     public Color32[] GetRow(int length)

5ba92035b86c01a89d026a7df05dc418d79970c6
Vsevolod Ivanov <vsevolod.ivanov@gmail.com>
2020-03-11 02:47:19 +0300

    refactoring

---
+++ udf/Assets/MusicButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicButton : MonoBehaviour
+{
+    int _counter;
+    public Button musicButton;
+    public Sprite musicOff;
+    public Sprite musicOn;
+    public GameObject controller;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("musicOff", 0) == 1)
+        {
+            GetComponent<Button>().onClick.Invoke();
+        }
+    }
+
+    public void SwitchButton()
+    {
+        _counter++;
+        if (_counter % 2 == 1)
+        {
+            musicButton.GetComponent<Image>().sprite = musicOff;
+            controller.GetComponent<AudioSource>().mute = true;
+            PlayerPrefs.SetInt("musicOff", 1);
+        }
+        else {
+            musicButton.GetComponent<Image>().sprite = musicOn;
+            controller.GetComponent<AudioSource>().mute = false;
+            PlayerPrefs.SetInt("musicOff", 0);
+        }
+    }
+}
---
+++ udf/Assets/SoundButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour
+{
+    int _counter;
+    public Button soundButton;
+    public Sprite soundOff;
+    public Sprite soundOn;
+    public GameObject projectile;
+    public GameObject balrog;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("sndOff", 0) == 1)
+        {
+            GetComponent<Button>().onClick.Invoke();
+        }
+    }
+    public void SwitchButton() {
+        _counter++;
+        if (_counter % 2 == 1)
+        {
+            soundButton.GetComponent<Image>().sprite = soundOff;
+            projectile.GetComponent<AudioSource>().mute = true;
+            balrog.GetComponent<AudioSource>().mute = true;
+            PlayerPrefs.SetInt("sndOff", 1);
+        }
+        else {
+            soundButton.GetComponent<Image>().sprite = soundOn;
+            projectile.GetComponent<AudioSource>().mute = false;
+            balrog.GetComponent<AudioSource>().mute = false;
+            PlayerPrefs.SetInt("sndOff", 0);
+        }
+    }
+}
